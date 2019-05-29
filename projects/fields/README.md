0fb97634b57c7456d64795841cf4d1a812e3e939
Danila Rukhovich <danrukh@gmail.com>
2019-05-29 11:59:08 +0300

    first commit for projects/fields

---
+++ projects/fields/README.md
@@ -0,0 +1,7 @@
+```
+<in_path>
+-- fields.*
+-- NDVI_list.xls
+-- NDVI_tif
+   -- *.tif
+```
---
+++ projects/fields/app.py
@@ -0,0 +1,113 @@
+import os
+import cv2
+import json
+import numpy as np
+import pandas as pd
+from osgeo import ogr, osr, gdal
+from argparse import ArgumentParser
+
+
+def make_full_mask(points):
+    x_min = points[:, 0].min()
+    y_max = points[:, 1].max()
+    mask_width = points[:, 0].max() - x_min
+    mask_height = y_max - points[:, 1].min()
+    width = int(mask_width // 10.0 + 1)
+    height = int(mask_height // 10.0 + 1)
+    mask = np.zeros((height, width), dtype=np.uint8)
+    points[:, 0] = (points[:, 0] - x_min) // 10.0
+    points[:, 1] = (points[:, 1] - y_max) // -10.0
+    cv2.fillPoly(mask, np.reshape(points, (1, len(points), 2)).astype(np.int32), 1)
+    return mask, x_min, y_max, mask_width, mask_height
+
+
+def erode(mask, size):
+    return mask
+
+
+def dilate(image, mask, size=3):
+    return image
+
+
+def _crop(image, x_mask_min, y_mask_max, mask_width, mask_height, x_min, y_max, x_step, y_step):
+    x_crop_min = int((x_mask_min - x_min) // x_step)
+    x_crop_max = int((x_mask_min - x_min) // x_step) + int(mask_width // x_step + 1)
+    y_crop_min = int((y_mask_max - y_max) // y_step)
+    y_crop_max = int((y_mask_max - y_max) // y_step) + int(mask_height // abs(y_step) + 1)
+    return image[y_crop_min: y_crop_max, x_crop_min: x_crop_max]
+
+
+def crop(tif_file, x_mask_min, y_mask_max, mask_width, mask_height, sensor):
+    x_min, _, _, y_max, _, _ = tif_file.GetGeoTransform()
+    full_image = tif_file.GetRasterBand(1).ReadAsArray()
+    if sensor[0] != 'S':
+        image = _crop(full_image, x_mask_min, y_mask_max, mask_width + 30, mask_height + 30, x_min, y_max, 30, -30)
+        image = cv2.resize(image, dsize=(image.shape[1] * 3, image.shape[0] * 3), interpolation=cv2.INTER_CUBIC)
+        x_min = x_min + (x_mask_min - x_min) // 30.0 * 30
+        y_max = y_max + (y_mask_max - y_max) // -30.0 * -30
+    else:
+        image = full_image
+    return _crop(image, x_mask_min, y_mask_max, mask_width, mask_height, x_min, y_max, 10, -10)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--in_path', type=str, required=True)
+    parser.add_argument('--buffer_size', type=int, default=0)
+    options = vars(parser.parse_args())
+
+    in_path = options['in_path']
+    config_path = os.path.join(in_path, 'config.json')
+    if os.path.exists(config_path):
+        config = json.loads(config_path)
+        options.update(config)
+    tif_path = os.path.join(in_path, 'NDVI_tif')
+    shape_path = os.path.join(in_path, 'fields.shp')
+    shape_file = ogr.Open(shape_path)
+    excel_path = os.path.join(in_path, 'NDVI_list.xls')
+    excel_file = pd.read_excel(excel_path)
+    out_path = os.path.join(in_path, 'out')
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
+    layer = shape_file.GetLayer(0)
+    for feature in layer:
+        field = json.loads(feature.ExportToJson())
+        points = np.array(field['geometry']['coordinates'][0])
+        name = field['properties']['name']
+        print(name)
+        full_mask, x_mask_min, y_mask_max, mask_width, mask_height = make_full_mask(points)
+        landsat_mask = erode(mask=full_mask, size=3)
+        sentinel_mask = erode(mask=full_mask, size=1)
+        images = []
+        sensors = []
+        for _, row in excel_file[excel_file['name'] == name].iterrows():
+            sensor = row['sensor']
+            tif_file_name = row['NDVI_map'][:-4] + '.tif'
+            tif_file = gdal.Open(os.path.join(tif_path, tif_file_name))
+            image = crop(
+                tif_file=tif_file, x_mask_min=x_mask_min, y_mask_max=y_mask_max,
+                mask_width=mask_width, mask_height=mask_height, sensor=sensor
+            )
+            images.append(image)
+            sensors.append(sensor)
+        deviations = np.empty((len(images), full_mask.shape[0], full_mask.shape[1]))
+        deviations.fill(np.nan)
+        for i in range(len(images)):
+            mask = sentinel_mask if sensors[i][0] == 'S' else landsat_mask
+            deviation = images[i]
+            deviation[np.where(np.logical_not(mask))] = np.nan
+            deviation -= np.nanmean(deviation)
+            deviations[i] = deviation
+        deviation = np.nanmean(deviations, axis=0)
+        deviation = dilate(image=deviation, mask=full_mask)
+        deviation[np.where(np.isnan(deviation))] = 0
+
+        driver = gdal.GetDriverByName('GTiff')
+        dataset = driver.Create(
+            os.path.join(out_path, f'{name}.tif'), full_mask.shape[1], full_mask.shape[0], 1, gdal.GDT_Float32
+        )
+        dataset.SetGeoTransform((x_mask_min, 10, 0, y_mask_max, 0, -10))
+        dataset.SetProjection(tif_file.GetSpatialRef().ExportToWkt())
+        dataset.GetRasterBand(1).WriteArray(deviation)
+        dataset.FlushCache()
