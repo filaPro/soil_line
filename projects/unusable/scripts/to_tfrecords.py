541b9c0388bc000fa6d234560832d62167261715
Danila Rukhovich <danrukh@gmail.com>
2020-02-24 10:13:12 +0300

    add unusable to_tfrecords script

---
+++ projects/unusable/scripts/to_tfrecords.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import pandas as pd
+from osgeo import gdal
+import tensorflow as tf
+from argparse import ArgumentParser
+
+from scripts.common import Writer
+from utils import read_masks
+
+
+def run(tif_path, shape_path, excel_path, out_path, tmp_path, n_records, resolution):
+    channels = {
+        'blue': ['01', '02'],
+        'green': ['02', '03'],
+        'red': ['03', '04'],
+        'nir': ['04', '05'],
+        'swir1': ['05', '06'],
+        'swir2': ['07', '07']
+    }
+    masks = read_masks(
+        shape_path=shape_path,
+        resolution=resolution
+    )
+    visited = set()
+    excel_file = pd.read_excel(excel_path)
+    writer = Writer(path=out_path, size=n_records)
+    writer.reset('')
+    for file_name in os.listdir(tif_path):
+        base_file_name = '_'.join(file_name.split('_')[:4])
+        channel_shift = base_file_name.split('_')[2][-1] == '8'
+        if base_file_name in visited:
+           continue
+        visited.add(base_file_name)
+        feature = {}
+        for i, channel in enumerate(channels.keys()):
+            tif_file_name = f'{base_file_name}_{channel}_{channels[channel][channel_shift]}.tif'
+            tif_file = gdal.Open(os.path.join(tif_path, tif_file_name))
+            x_min, _, _, y_max, _, _ = tif_file.GetGeoTransform()
+            image = tif_file.GetRasterBand(1).ReadAsArray()
+            x_max = x_min + resolution * image.shape[1]
+            y_min = y_max - resolution * image.shape[0]
+            feature[f'channels/{i}'] = tf.train.Feature(float_list=tf.train.FloatList(value=np.ravel(image)))
+        feature['x_min'] = tf.train.Feature(float_list=tf.train.FloatList(value=(x_min,)))
+        feature['y_max'] = tf.train.Feature(float_list=tf.train.FloatList(value=(y_max,)))
+        feature['width'] =  tf.train.Feature(int64_list=tf.train.Int64List(value=(image.shape[1],)))
+        feature['height'] = tf.train.Feature(int64_list=tf.train.Int64List(value=(image.shape[0],)))
+        positive_names = excel_file[excel_file['NDVI_map'] == f'{base_file_name}_n.tif']['name'].values
+        negative_names = []
+        for name in masks.keys():
+            if not name in positive_names and x_min < masks[name]['x'] < x_max and y_min < masks[name]['y'] < y_max:
+                negative_names.append(name)
+        feature['positive'] = tf.train.Feature(int64_list=tf.train.Int64List(
+            value=[masks[name]['id'] for name in positive_names])
+        )
+        feature['negative'] = tf.train.Feature(int64_list=tf.train.Int64List(
+            value=[masks[name]['id'] for name in negative_names])
+        )
+        writer.write(tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString())
+    writer.close()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--in-path', type=str, default='/data')
+    parser.add_argument('--out-path', type=str, default='/volume/unusable_tfrecords')
+    parser.add_argument('--tmp-path', type=str, default='/tmp/tmp.tif')
+    parser.add_argument('--n-records', type=int, default=10)
+    parser.add_argument('--resolution', type=float, default=30.)
+    options = vars(parser.parse_args())
+
+    run(
+        tif_path=os.path.join(options['in_path'], 'CH'),
+        shape_path=os.path.join(options['in_path'], 'fields.shp'),
+        excel_path=os.path.join(options['in_path'], 'NDVI_list.xls'),
+        out_path=options['out_path'],
+        tmp_path=options['tmp_path'],
+        n_records=options['n_records'],
+        resolution=options['resolution']
+    )
+
+
---
+++ projects/unusable/utils.py
@@ -0,0 +1,57 @@
+import cv2
+import json
+import numpy as np
+from osgeo import ogr
+
+
+def depth(l):
+    return ((max(map(depth, l)) + 1) if l else 1) if isinstance(l, list) else 0
+
+
+def reshape_points(points):
+    if depth(points) == 3:
+        return [points]
+    return points
+
+
+def make_cropped_mask(points, resolution, x_min, y_max, height, width):
+    points = reshape_points(points)
+    mask = np.zeros((height, width), dtype=np.uint8)
+    for subfield in points:
+        for i, contour in enumerate(subfield):
+            contour = np.array(contour)[:, :2]
+            contour[:, 0] = (contour[:, 0] - x_min) // resolution
+            contour[:, 1] = (y_max - contour[:, 1]) // resolution
+            color = 1 if i == 0 else 0
+            cv2.fillPoly(mask, np.reshape(contour, (1, len(contour), 2)).astype(np.int32), color)
+    return mask
+
+
+def make_mask(points, resolution):
+    points = reshape_points(points)
+    concatenated = np.concatenate([np.concatenate(p) for p in points])
+    x_min = concatenated[:, 0].min() - resolution
+    y_max = concatenated[:, 1].max() + resolution
+    mask_width = concatenated[:, 0].max() - x_min + resolution
+    mask_height = y_max - concatenated[:, 1].min() + resolution
+    width = int(mask_width // resolution + 1)
+    height = int(mask_height // resolution + 1)
+    mask = make_cropped_mask(points, resolution, x_min, y_max, height, width)
+    return mask, x_min, y_max, mask_width, mask_height
+
+
+def read_masks(shape_path, resolution):
+    shape_file = ogr.Open(shape_path)
+    layer = shape_file.GetLayer(0)
+    masks = {}
+    for i, feature in enumerate(layer):
+        field = json.loads(feature.ExportToJson())
+        points = field['geometry']['coordinates']
+        mask, x_mask_min, y_mask_max, mask_width, mask_height = make_mask(points, resolution)
+        masks[field['properties']['name']] = {
+            'id': field['properties']['id'] - 1,
+            'mask': mask,
+            'x': x_mask_min + mask_width / 2,
+            'y': y_mask_max - mask_height / 2
+        }
+    return masks
