3b382079106304d2aeea31330464c86fdb630eef
Danila Rukhovich <danrukh@gmail.com>
2020-02-27 09:01:28 +0300

    looks like .93 accuracy on validation

--- projects/unusable/dataset.py
+++ projects/unusable/dataset.py
@@ -15,9 +15,9 @@ def parse_example(example, n_channels):
         'negative': tf.io.VarLenFeature(tf.int64),
     }
     for i in range(n_channels):
-        feature[f'channel/{i}'] = tf.io.VarLenFeature(tf.float32)
+        feature[f'channels/{i}'] = tf.io.VarLenFeature(tf.float32)
     data = tf.io.parse_single_example(example, feature)
-    keys = [f'channel/{i}' for i in range(n_channels)] + ['positive', 'negative']
+    keys = [f'channels/{i}' for i in range(n_channels)] + ['positive', 'negative']
     for key in keys:
         data[key] = tf.sparse.to_dense(data[key])
     return data
@@ -27,7 +27,7 @@ def list_tfrecords(path):
     return list(os.path.join(path, name) for name in os.listdir(path))
 
 
-def make_class_dataset(paths, transform_lambda, label, n_processes=N_PROCESSES):
+def make_class_dataset(paths, transform_lambda, n_channels, label, n_processes=N_PROCESSES):
     return tf.data.Dataset.from_tensor_slices(
         paths
     ).interleave(
@@ -35,20 +35,20 @@ def make_class_dataset(paths, transform_lambda, label, n_processes=N_PROCESSES):
         cycle_length=n_processes,
         num_parallel_calls=n_processes
     ).map(
-        parse_example,
+        partial(parse_example, n_channels=n_channels),
         num_parallel_calls=n_processes
+    ).filter(
+        lambda x: len(x[label]) > 0
     ).map(
         partial(transform_lambda, label=label),
         num_parallel_calls=n_processes
-    ).filter(
-        lambda x: x['status']
     ).repeat(
     )
 
 
-def make_dataset(paths, transform_lambda, batch_size, buffer_size, n_processes=N_PROCESSES):
-    positive = make_class_dataset(paths, transform_lambda, 'positive')
-    negative = make_class_dataset(paths, transform_lambda, 'negative')
+def make_dataset(paths, transform_lambda, n_channels, batch_size, buffer_size, n_processes=N_PROCESSES):
+    positive = make_class_dataset(paths, transform_lambda, n_channels, 'positive')
+    negative = make_class_dataset(paths, transform_lambda, n_channels, 'negative')
     return tf.data.experimental.sample_from_datasets(
         (positive, negative)
     ).shuffle(
@@ -61,47 +61,40 @@ def make_dataset(paths, transform_lambda, batch_size, buffer_size, n_processes=N
 
 
 def crop_or_pad(image, x, y, size):
-    image = tf.concat((image, mask), axis=-1)
-    half_size = tf.cast(size / 2, tf.int64)
+    half_size = tf.cast(size / 2, tf.int32)
     left_pad = -tf.minimum(x - half_size, 0)
     top_pad = -tf.minimum(y - half_size, 0)
-    right_pad = -tf.minimum(image.shape[1] - (left_pad + size), 0)
-    bottom_pad = -tf.minimum(image.shape[0] - (top_pad + size), 0)
+    right_pad = -tf.minimum(tf.shape(image)[1] - x - half_size, 0)
+    bottom_pad = -tf.minimum(tf.shape(image)[0] - y - half_size, 0)
     image = tf.pad(image, [[top_pad, bottom_pad], [left_pad, right_pad]])
-    return tf.image.crop_to_bounding_box(image, top_pad + y - half_size, left_pad + x - half_size, size, size)
-
+    return tf.image.crop_to_bounding_box(
+        image[..., tf.newaxis], top_pad + y - half_size, left_pad + x - half_size, size, size
+    )[..., 0]
 
 
 def transform(data, label, masks, xs, ys, n_channels, size, resolution):
     channels = [tf.reshape(data[f'channels/{i}'], (data['height'], data['width'])) for i in range(n_channels)]
-    n_fields = len(data[label])
-    if not n_fields:
-        return {'status': False}
-    i = tf.random.uniform((), 0, n_fields, dtype=tf.int64)
-    mask = tf.io.decode_png(masks[i], channels=1)
-    image = tf.concat(
+    i = tf.random.uniform((), 0, len(data[label]), dtype=tf.int32)
+    mask = tf.io.decode_png(masks[i], channels=1)[..., 0]
+    image = tf.stack(
         [crop_or_pad(
             image=channels[i],
-            x=tf.cast((xs[i] - data['x_min']) / resolution, tf.int64), 
-            y=tf.cast((data['y_max'] - ys[i]) / resolution, tf.int64),
+            x=tf.cast((xs[i] - data['x_min']) / resolution, tf.int32),
+            y=tf.cast((data['y_max'] - ys[i]) / resolution, tf.int32),
             size=size
         ) for i in range(n_channels)] +
         [crop_or_pad(
             image=tf.cast(mask, tf.float32),
-            x=tf.cast((mask.shape[1] / 2, tf.int64) / resolution, tf.int64),
-            y=tf.cast((mask.shape[0] / 2, tf.int64) / resolution, tf.int64),
+            x=tf.cast(tf.shape(mask)[1] / 2, tf.int32),
+            y=tf.cast(tf.shape(mask)[0] / 2, tf.int32),
             size=size
         )] + 
         [crop_or_pad(
             image=tf.ones_like(mask, tf.float32),
-            x=tf.cast((mask.shape[1] / 2, tf.int64) / resolution, tf.int64),
-            y=tf.cast((mask.shape[0] / 2, tf.int64) / resolution, tf.int64),
+            x=tf.cast(tf.shape(mask)[1] / 2, tf.int32),
+            y=tf.cast(tf.shape(mask)[0] / 2, tf.int32),
             size=size
         )],
-        axis=0
+        axis=-1
     )
-    return {
-        'image': image,
-        'label': label == 'positive'
-    }
-
+    return image, label == 'positive'
--- projects/unusable/runners/run.py
+++ projects/unusable/runners/run.py
@@ -1,3 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from functools import partial
+from argparse import ArgumentParser
+
+from utils import read_masks
+from dataset import list_tfrecords, make_dataset, transform
+
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser()
+    parser.add_argument('--in-path', type=str, default='/volume/unusable_tfrecords')
+    parser.add_argument('--shape-path', type=str, default='/data/fields.shp')
+    parser.add_argument('--out-path', type=str, default='/volume/logs/tmp')
+    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--resolution', type=float, default=30.)
+    options = vars(parser.parse_args())
+
+    size = 512
+    paths = list_tfrecords(options['in_path'])
+    n_training_paths = int(len(paths) * .8)
+    training_paths = paths[:n_training_paths]
+    validation_paths = paths[n_training_paths:]
+
+    masks_data = read_masks(
+        shape_path=options['shape_path'],
+        resolution=options['resolution']
+    )
+    masks, xs, ys = [], [], []
+    for _, item in masks_data.items():
+        masks.append(tf.image.encode_png(item['mask'][..., np.newaxis]))
+        xs.append(item['x'])
+        ys.append(item['y'])
+    transform_lambda = partial(
+        transform,
+        masks=tf.stack(masks),
+        xs=tf.cast(tf.stack(xs), tf.float32),
+        ys=tf.cast(tf.stack(ys), tf.float32),
+        n_channels=6,
+        size=size,
+        resolution=options['resolution']
+    )
+    dataset_lambda = partial(
+        make_dataset,
+        transform_lambda=transform_lambda,
+        n_channels=6,
+        batch_size=options['batch_size'],
+        buffer_size=8,
+    )
+    training_dataset = dataset_lambda(paths=training_paths)
+    validation_dataset = dataset_lambda(paths=validation_paths)
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(size, size, 6 + 2)),
+        tf.keras.layers.Conv2D(32, 3, 2, activation='relu'),
+        tf.keras.layers.Conv2D(64, 3, 2, activation='relu'),
+        tf.keras.layers.Conv2D(128, 3, 2, activation='relu'),
+        tf.keras.layers.Conv2D(256, 3, 2, activation='relu'),
+        tf.keras.layers.Conv2D(512, 3, 2, activation='relu'),
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+    history = model.fit_generator(
+        training_dataset,
+        steps_per_epoch=100,
+        epochs=100,
+        validation_data=validation_dataset,
+        validation_steps=100
+    )
